c0830becca0efb7a0bc956ead8d1c9a1a60a6c59
thierry <thierry.henrio@gmail.com>
2010-12-15 16:03:03 +0100

    move and extracted code

--- lib/cant.rb
+++ lib/cant.rb
@@ -1,3 +1,36 @@
 module Cant
-  # Your code goes here...
+  module Backend
+    class Code
+      # return true if any rule is true
+      # else raise Cant::Unauthorized
+      def can?(context)
+        raise Cant::Unauthorized.new(%{can't you do that?\n#{context}}) if rules.empty?
+        true
+      end
+
+      # add a rule that when context is met, then response is what block evaluates to
+      def can(context, &block)
+        rules << Rule.new(context, &block)
+      end
+
+      private
+      def rules
+        @rules ||= []
+      end
+
+      class Rule
+        def initialize(context={}, &block)
+          @context = context
+          @block = block
+        end
+        def can?(context={})
+          self.instance_eval {
+            return @block.call(context)
+          }
+        end
+      end
+    end
+  end
+
+  class Unauthorized < RuntimeError; end
 end
--- spec/cant_spec.rb
+++ spec/cant_spec.rb
@@ -1,43 +1,5 @@
 require 'spec_helper'
-
-module Cant
-  module Backend
-    class Code
-      # return true if any rule is true
-      # else raise Cant::Unauthorized
-      def can?(context)
-        raise Cant::Unauthorized.new(%{can't you do that?\n#{context}}) if rules.empty?
-        true
-      end
-
-      # add a rule that when context is met, then response is what block evaluates to
-      def can(context, &block)
-        rules << Rule.new(context, &block)
-      end
-
-      private
-      def rules
-        @rules ||= []
-      end
-
-      class Rule
-        def initialize(context={}, &block)
-          @context = context
-          @block = block
-        end
-        def can?(context={})
-          self.instance_eval {
-            return @block.call(context)
-          }
-        end
-      end
-    end
-  end
-
-  class Unauthorized < RuntimeError; end
-end
-
-
+require 'cant'
 describe Cant::Backend::Code do
   let(:backend) {Cant::Backend::Code.new}
 
