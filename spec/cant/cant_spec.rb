87c8d8f2bf87b17ba9e032dbc6d11f14ab7b4de8
thierry <thierry.henrio@gmail.com>
2010-12-15 12:04:57 +0100

    can do things

--- spec/cant/cant_spec.rb
+++ spec/cant/cant_spec.rb
@@ -4,8 +4,27 @@ module Cant
   module Backend
     class Code
       def can?(user, perform, something)
-        raise Cant::Unauthorized.new(%{can't you do that?})
+        raise Cant::Unauthorized.new(%{can't you do that?}) if rules.empty?
+        true
       end
+      
+      def can(user, perform, something)
+        rules << :pop
+      end
+      
+      private
+      def rules
+        @rules ||= []
+      end
+      
+      # class Rule
+      #   def initialize(who, can_do, what, &block)
+      #     @who = who
+      #     @can_do
+      #   end
+      #    def can?(user, perform, something)
+      #    end
+      # end
     end
   end
 
@@ -23,6 +42,13 @@ describe Cant::Backend::Code do
       }
       assert {e.kind_of?(Cant::Unauthorized)}
       assert {e.message =~ /^can't you do that?/}
+    end
+  end
+  
+  context "with one rule" do
+    it 'can do when at least one rule enables' do
+      backend.can(:fred, :do, :thing)
+      assert {backend.can?(:fred, :do, :thing) == true}
     end
   end
 end
