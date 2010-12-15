c3a7cc2e00de17f068452751de09fde99618e7c4
thierry <thierry.henrio@gmail.com>
2010-12-15 11:47:26 +0100

    default raise

--- spec/cant/cant_spec.rb
+++ spec/cant/cant_spec.rb
@@ -4,15 +4,25 @@ module Cant
   module Backend
     class Code
       def can?(user, perform, something)
+        raise Cant::Unauthorized.new(%{can't you do that?})
       end
     end
   end
+
+  class Unauthorized < RuntimeError; end
 end
 
 
-describe Cant::Backend::Code do  
-  it "cant do a thing" do
-    backend = Cant::Backend::Code.new
-    backend.can?(nil, :do, :thing)
+describe Cant::Backend::Code do
+  let(:backend) {Cant::Backend::Code.new}
+  
+  context "empty, no rulez" do
+    it "raises!" do
+      e = rescuing {
+        backend.can?(nil, :do, :thing)
+      }
+      assert {e.kind_of?(Cant::Unauthorized)}
+      assert {e.message =~ /^can't you do that?/}
+    end
   end
 end
